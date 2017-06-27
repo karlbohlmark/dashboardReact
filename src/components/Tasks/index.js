859e0beb077c7725c528f817264e73a3e97f7693
Evgeny Shmakov <sjs-master@yandex.ru>
2017-06-27 23:26:01 +0700

    add highlights to task page

--- src/components/Tasks/index.js
+++ src/components/Tasks/index.js
@@ -18,6 +18,78 @@ function Tasks(props) {
     return (
         <div styleName='root'>
             <div styleName='users_container'>
+                <div styleName='user_container_header'>HIGHLIGHTS</div>
+                <div styleName="returning_subscribers">
+                    <div>
+                        <div styleName='sub_container_header'>COMPLETED TASKS</div>
+                        <div styleName="list_column">
+                        </div>
+                    </div>
+                    <div styleName="list_column" style={{marginLeft: 20}}>
+                        <div styleName="list_row_reverse">
+                            <div styleName="list_item_number">165</div>
+                            <div styleName="list_columnItemStart">
+                                <div styleName="list_column_item"> NUMBER OF TASKS REPORTED</div>
+                                <div styleName="list_column_itemSmall">(FOR SELECTED PERIOD)</div>
+                            </div>
+                        </div>
+                        <div styleName="list_row_reverse">
+                            <div styleName="list_item_number">38</div>
+                            <div styleName="list_columnItemStart">
+                                <div styleName="list_column_item"> ASSIGNED TASKS</div>
+                                <div styleName="list_column_itemSmall">
+                                    (ASSIGNED TO GOFUNDI BUT PENDING COMPLETION)
+                                </div>
+                            </div>
+                        </div>
+                        <div styleName="list_row_reverse">
+                            <div styleName="list_item_number">18</div>
+                            <div styleName="list_columnItemStart">
+                                <div styleName="list_column_item"> NUMBER OF DECLINES</div>
+                                <div styleName="list_column_itemSmall">(FOR SELECTED PERIOD)</div>
+                            </div>
+                        </div>
+                        <div styleName="list_row_reverse">
+                            <div styleName="list_item_number_pink">45 sec</div>
+                            <div styleName="list_columnItemStart">
+                                <div styleName="list_column_item"> AVERAGE TIME FOR ASSINMENT</div>
+                                <div styleName="list_column_itemSmall">(FROM REQUESTED ACCEPTED)</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div styleName="list_column" style={{marginLeft: 20}}>
+                        <div styleName="list_row_reverse">
+                            <div styleName="list_item_number">2</div>
+                            <div styleName="list_columnItemStart">
+                                <div styleName="list_column_item"> UNASSIGNED TASKS</div>
+                                <div styleName="list_column_itemSmall">(TASK NOT YET ACCEPTED BY GOFUNDI)</div>
+                            </div>
+                        </div>
+                        <div styleName="list_row_reverse">
+                            <div styleName="list_item_number">15%</div>
+                            <div styleName="list_columnItemStart">
+                                <div styleName="list_column_item"> INCREASE SINCE LAST MONTH</div>
+                                <div styleName="list_column_itemSmall">(TASKS COMPLETED)</div>
+                            </div>
+                        </div>
+                        <div styleName="list_row_reverse">
+                            <div styleName="list_item_number">5</div>
+                            <div styleName="list_columnItemStart">
+                                <div styleName="list_column_item"> NUMBER OF CANCELLATIONS</div>
+                                <div styleName="list_column_itemSmall">(FOR SELECTED PERIOD)</div>
+                            </div>
+                        </div>
+                        <div styleName="list_row_reverse">
+                            <div styleName="list_item_number_pink">1 hr</div>
+                            <div styleName="list_columnItemStart">
+                                <div styleName="list_column_item"> AVERAGE TIME FOR COMPLETION</div>
+                                <div styleName="list_column_itemSmall">(FROM REQUESTED TO COMPLETED)</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div styleName='users_container'>
                 <div styleName='user_container_header'>TASK STATUS</div>
                 <SelectBoxItem
                     options={[
