dd1d7ab7cebe9eb35175a2662053af0eec6ec565
Evgeny Shmakov <sjs-master@yandex.ru>
2017-06-29 11:25:22 +0700

    customize subscribtion area charts

--- src/components/Subscribers/index.js
+++ src/components/Subscribers/index.js
@@ -90,17 +90,18 @@ function Subscribers(props) {
                             </div>
                         </div>
                     </div>
-                    <div>
+                    <div style={{width: '270px'}}>
                         <div styleName='sub_container_header'>SUBSCRIBERS SHARE PER AREA</div>
                         <div styleName="list_column_highcharts" style={{margin: 5}}>
                             <Highchart config={{
                                 credits: {
                                     enabled: false
                                 },
+                                colors: ['#cc6631'],
                                 chart: {
                                     type: 'column',
-                                    width: 180,
-                                    height: 180
+                                    width: 230,
+                                    height: 230
                                 },
                                 title: {
                                     text: ''
@@ -124,230 +125,42 @@ function Subscribers(props) {
                                     series: {
                                         borderWidth: 0,
                                         dataLabels: {
-                                            enabled: true,
-                                            format: '{point.y:.1f}%'
+                                            enabled: true
+                                            // format: '{point.y:.1f}%'
                                         }
                                     }
                                 },
 
                                 tooltip: {
                                     headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
-                                    pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>'
+                                    pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}</b><br/>'
                                 },
 
                                 series: [{
-                                    name: 'Brands',
-                                    colorByPoint: true,
+                                    name: 'Area',
+                                    colorByPoint: false,
                                     data: [{
-                                        name: 'Microsoft Internet Explorer',
-                                        y: 56.33,
-                                        drilldown: 'Microsoft Internet Explorer'
+                                        name: 'Livington',
+                                        y: 15800,
+                                        drilldown: null
                                     }, {
-                                        name: 'Chrome',
-                                        y: 24.03,
-                                        drilldown: 'Chrome'
+                                        name: 'Westlands',
+                                        y: 5323,
+                                        drilldown: null
                                     }, {
-                                        name: 'Firefox',
-                                        y: 10.38,
-                                        drilldown: 'Firefox'
+                                        name: 'Kibra',
+                                        y: 2963,
+                                        drilldown: null
                                     }, {
-                                        name: 'Safari',
-                                        y: 4.77,
-                                        drilldown: 'Safari'
+                                        name: 'Kiambu',
+                                        y: 1286,
+                                        drilldown: null
                                     }, {
-                                        name: 'Opera',
-                                        y: 0.91,
-                                        drilldown: 'Opera'
-                                    }, {
-                                        name: 'Proprietary or Undetectable',
-                                        y: 0.2,
+                                        name: 'Ruaka',
+                                        y: 3265,
                                         drilldown: null
                                     }]
-                                }],
-                                drilldown: {
-                                    series: [{
-                                        name: 'Microsoft Internet Explorer',
-                                        id: 'Microsoft Internet Explorer',
-                                        data: [
-                                            [
-                                                'v11.0',
-                                                24.13
-                                            ],
-                                            [
-                                                'v8.0',
-                                                17.2
-                                            ],
-                                            [
-                                                'v9.0',
-                                                8.11
-                                            ],
-                                            [
-                                                'v10.0',
-                                                5.33
-                                            ],
-                                            [
-                                                'v6.0',
-                                                1.06
-                                            ],
-                                            [
-                                                'v7.0',
-                                                0.5
-                                            ]
-                                        ]
-                                    }, {
-                                        name: 'Chrome',
-                                        id: 'Chrome',
-                                        data: [
-                                            [
-                                                'v40.0',
-                                                5
-                                            ],
-                                            [
-                                                'v41.0',
-                                                4.32
-                                            ],
-                                            [
-                                                'v42.0',
-                                                3.68
-                                            ],
-                                            [
-                                                'v39.0',
-                                                2.96
-                                            ],
-                                            [
-                                                'v36.0',
-                                                2.53
-                                            ],
-                                            [
-                                                'v43.0',
-                                                1.45
-                                            ],
-                                            [
-                                                'v31.0',
-                                                1.24
-                                            ],
-                                            [
-                                                'v35.0',
-                                                0.85
-                                            ],
-                                            [
-                                                'v38.0',
-                                                0.6
-                                            ],
-                                            [
-                                                'v32.0',
-                                                0.55
-                                            ],
-                                            [
-                                                'v37.0',
-                                                0.38
-                                            ],
-                                            [
-                                                'v33.0',
-                                                0.19
-                                            ],
-                                            [
-                                                'v34.0',
-                                                0.14
-                                            ],
-                                            [
-                                                'v30.0',
-                                                0.14
-                                            ]
-                                        ]
-                                    }, {
-                                        name: 'Firefox',
-                                        id: 'Firefox',
-                                        data: [
-                                            [
-                                                'v35',
-                                                2.76
-                                            ],
-                                            [
-                                                'v36',
-                                                2.32
-                                            ],
-                                            [
-                                                'v37',
-                                                2.31
-                                            ],
-                                            [
-                                                'v34',
-                                                1.27
-                                            ],
-                                            [
-                                                'v38',
-                                                1.02
-                                            ],
-                                            [
-                                                'v31',
-                                                0.33
-                                            ],
-                                            [
-                                                'v33',
-                                                0.22
-                                            ],
-                                            [
-                                                'v32',
-                                                0.15
-                                            ]
-                                        ]
-                                    }, {
-                                        name: 'Safari',
-                                        id: 'Safari',
-                                        data: [
-                                            [
-                                                'v8.0',
-                                                2.56
-                                            ],
-                                            [
-                                                'v7.1',
-                                                0.77
-                                            ],
-                                            [
-                                                'v5.1',
-                                                0.42
-                                            ],
-                                            [
-                                                'v5.0',
-                                                0.3
-                                            ],
-                                            [
-                                                'v6.1',
-                                                0.29
-                                            ],
-                                            [
-                                                'v7.0',
-                                                0.26
-                                            ],
-                                            [
-                                                'v6.2',
-                                                0.17
-                                            ]
-                                        ]
-                                    }, {
-                                        name: 'Opera',
-                                        id: 'Opera',
-                                        data: [
-                                            [
-                                                'v12.x',
-                                                0.34
-                                            ],
-                                            [
-                                                'v28',
-                                                0.24
-                                            ],
-                                            [
-                                                'v27',
-                                                0.17
-                                            ],
-                                            [
-                                                'v29',
-                                                0.16
-                                            ]
-                                        ]
-                                    }]
-                                }
+                                }]
                             }} />
                         </div>
                     </div>
