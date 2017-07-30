1bc02633057a3d0f4a714412a677348858014878
Evgeny Shmakov <sjs-master@yandex.ru>
2017-07-30 19:45:34 +0700

    add CategoryItem component; fix styles; add props

---
+++ src/components/TasksCategoryBreakdown/CategoryItem.js
@@ -0,0 +1,31 @@
+import React, { PropTypes } from 'react';
+import CSSModules from 'react-css-modules';
+import styles from './styles.css';
+import {
+    isArray
+} from 'lodash/fp';
+import CircularChartItem from 'components/CircularChart/CircularChartItem';
+
+
+function CategoryItem(props) {
+    return (
+        <div styleName='container_category_item'>
+            <div styleName='container_header_sub'>
+                {(props.category && props.category.name) ? props.category.name.toString().toUpperCase() : ''}
+            </div>
+            <div styleName='container_middle'>
+                {isArray(props.subCategories) ? props.subCategories.map((subCat, item) => (
+                    <CircularChartItem key={item} {...subCat} />
+                )) : null}
+            </div>
+            <div styleName='container_bottom'/>
+        </div>
+    );
+}
+
+CategoryItem.propTypes = {
+    category: PropTypes.object.isRequired,
+    subCategories: PropTypes.array.isRequired,
+};
+
+export default CSSModules(CategoryItem, styles);
--- src/components/TasksCategoryBreakdown/index.js
+++ src/components/TasksCategoryBreakdown/index.js
@@ -4,7 +4,7 @@ import styles from './styles.css';
 import {
     isArray
 } from 'lodash/fp';
-import CircularChartItem from 'components/CircularChart/CircularChartItem';
+import CategoryItem from 'components/TasksCategoryBreakdown/CategoryItem';
 import Placeholder from 'components/Placeholder';
 
 
@@ -18,51 +18,12 @@ function TasksCategoryBreakdown(props) {
                     ),
                     Just: fields => (
                         <div styleName="returning_subscribers">
-                            <div styleName="reparate_item_col" style={{
-                                textAlign: 'center',
-                                height: '72px',
-                                display: 'flex',
-                                width: '10%',
-                                flexDirection: 'column',
-                                alignSelf: 'flex-end',
-                                backgroundColor: '#8f9092'
-                            }}>
+                            <div styleName="reparate_item_col">
                                 <div styleName="small_text_panel">REPORTED</div>
                                 <div styleName="small_text_panel">COMPLETED</div>
                             </div>
                             {isArray(fields) ? fields.map((field, index) => (
-                                <div style={{
-                                    textAlign: 'center',
-                                    alignItems: 'center',
-                                    width: '50%',
-                                    display: 'flex',
-                                    flexDirection: 'column',
-                                    position: 'relative'
-                                }}
-                                     key={index}>
-                                    <div styleName='container_header_sub'>
-                                        {(field.category && field.category.name) ?
-                                            field.category.name.toString().toUpperCase() : ''}
-                                    </div>
-                                    <div style={{
-                                        display: 'flex',
-                                        flexDirection: 'row',
-                                        zIndex: 1
-                                    }}>
-                                        {isArray(field.subCategories) ? field.subCategories.map((subCat, item) => (
-                                            <CircularChartItem key={item} {...subCat} />
-                                        )) : null}
-                                    </div>
-                                    <div style={{
-                                        position: 'absolute',
-                                        width: '100%',
-                                        zIndex: 0,
-                                        left: 0,
-                                        bottom: 0,
-                                        backgroundColor: 'rgb(88, 88, 90)',
-                                        height: '72px'}}>
-                                    </div>
-                                </div>
+                                <CategoryItem key={index} {...field} />
                             )) : null}
                         </div>
                     )
