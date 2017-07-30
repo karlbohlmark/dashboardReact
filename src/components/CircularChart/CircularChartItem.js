29ed16a17bf124522bec25bda64c0a733a4cb247
Evgeny Shmakov <sjs-master@yandex.ru>
2017-07-30 19:52:07 +0700

    class to func

--- src/components/CircularChart/CircularChartItem.js
+++ src/components/CircularChart/CircularChartItem.js
@@ -1,44 +1,43 @@
-import React, { PropTypes, Component } from 'react';
+import React, { PropTypes } from 'react';
 import CircularChart from 'components/CircularChart';
 import CSSModules from 'react-css-modules';
 import styles from './styles.css';
 
-class CircularChartItem extends Component {
-    render() {
-        return (
-            <CircularChart
-                className={(this.props.category && this.props.category.shortStyle) ?
-                    this.props.category.shortStyle : null}
-                percentage={this.props.percentage ? this.props.percentage : 0}
-                strokeWidth={5}
-                upder={
-                    <div styleName="reparate_item_col"
-                         style={{width: '100%', backgroundColor: '#58585a'}}>
-                        <div styleName="small_text_panel">
-                            {this.props.reported ?
-                                this.props.reported.toString().toUpperCase() : ''
-                            }
-                        </div>
-                        <div styleName="small_text_panel">
-                            {this.props.completed ?
-                                this.props.completed.toString().toUpperCase() : ''
-                            }
-                        </div>
+function CircularChartItem(props) {
+    return (
+        <CircularChart
+            className={(props.category && props.category.shortStyle) ?
+                props.category.shortStyle : null}
+            percentage={props.percentage ? props.percentage : 0}
+            strokeWidth={5}
+            upder={
+                <div styleName="reparate_item_col"
+                     style={{width: '100%', backgroundColor: '#58585a'}}>
+                    <div styleName="small_text_panel">
+                        {props.reported ?
+                            props.reported.toString().toUpperCase() : ''
+                        }
                     </div>
-                }
-            >
-                <div styleName={(this.props.category && this.props.category.style) ?
-                    this.props.category.style : 'new-installation-decoder'
-                } />
-                <div styleName="small_text">
-                    {(this.props.category && this.props.category.shortName) ?
-                        this.props.category.shortName.toString().toUpperCase() : ''
-                    }
+                    <div styleName="small_text_panel">
+                        {props.completed ?
+                            props.completed.toString().toUpperCase() : ''
+                        }
+                    </div>
                 </div>
-            </CircularChart>
-        );
-    }
+            }
+        >
+            <div styleName={(props.category && props.category.style) ?
+                props.category.style : 'new-installation-decoder'
+            } />
+            <div styleName="small_text">
+                {(props.category && props.category.shortName) ?
+                    props.category.shortName.toString().toUpperCase() : ''
+                }
+            </div>
+        </CircularChart>
+    );
 }
+
 CircularChartItem.propTypes = {
     category: PropTypes.object.isRequired,
     percentage: PropTypes.number.isRequired,
