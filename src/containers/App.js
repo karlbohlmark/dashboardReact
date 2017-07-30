0acb8f7318c915dd0229ea12dd680eaef4fa21b7
Evgeny Shmakov <sjs-master@yandex.ru>
2017-07-30 22:20:41 +0700

    wrap AppContainer

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,17 @@
-import React, { Component } from 'react';
-
+import React, { Component, PropTypes } from 'react';
+import {
+    withRouter
+} from 'react-router';
+import {
+    routerShape,
+    locationShape
+} from 'react-router/lib/PropTypes';
+import {
+    connect
+} from 'react-redux';
+import {
+    compose
+} from 'lodash/fp';
 import Page from 'components/Page';
 
 class AppContainer extends Component {
@@ -10,4 +22,22 @@ class AppContainer extends Component {
     }
 }
 
-export default AppContainer;
+AppContainer.propTypes = {
+    router: routerShape.isRequired,
+    location: locationShape.isRequired
+};
+
+function select({ ui }) {
+
+    return {
+    };
+}
+
+const bindActions = {
+
+};
+
+export default compose(
+    connect(select, bindActions),
+    withRouter
+)(AppContainer);
