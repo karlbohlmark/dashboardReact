2dafc7d626d6a782ffd72b8f410648c4906f0270
Evgeny Shmakov <sjs-master@yandex.ru>
2017-07-31 14:33:28 +0700

    add comp BlockStats; clear style; restruct component

--- src/components/Overview/index.js
+++ src/components/Overview/index.js
@@ -1,19 +1,15 @@
 import React, { PropTypes } from 'react';
 import CSSModules from 'react-css-modules';
 import styles from './styles.css';
-import {
-    isNull
-} from 'lodash/fp';
 import CompletedTasksHightChart from 'components/CompletedTasksHightChart';
+import OverviewBlockStats from 'components/Overview/BlockStats';
 import GofundisHightChart from 'components/GoFundis/HightChart';
 import TasksHistogram from 'components/Tasks/Histogram';
 import TasksStatusMap from 'components/Tasks/StatusMap';
 import CategoriesMap from 'components/CategoriesMap';
 import UsersMap from 'components/UsersMap';
-import ReportRow from 'components/ListItem/ReportRow';
 import Substrate from 'components/Substrate';
 import SubPanel from 'components/SubPanel';
-import Placeholder from 'components/Placeholder';
 
 function Overview(props) {
 
@@ -30,100 +26,7 @@ function Overview(props) {
             <div styleName='root'>
                 <div styleName='users_container_empty'>
                     <div styleName="returning_subscribers">
-                        <div style={{
-                            display: 'flex',
-                            flexDirection: 'row',
-                            alignSelf: 'center'
-                        }}>
-                            <div styleName="list_column" style={{marginLeft: 0}}>
-                                {props.getOverviewStats.errors.cata({
-                                    Nothing: () => props.getOverviewStats.results.cata({
-                                        Nothing: () => (
-                                            <Placeholder
-                                                style={{width: 220, height: 190}}
-                                                busy={props.getOverviewStats.busy}
-                                                size={[ '220px', '190px' ]} />
-                                        ),
-                                        Just: fields => (
-                                            <div>
-                                                <ReportRow
-                                                    styleChildren={{backgroundColor: '#07944a'}}
-                                                    styleUpItem={{textOverflow: 'ellipsis', overflow: 'hidden'}}
-                                                    upItem={
-                                                        fields.completedTasksPerDay &&
-                                                        !isNull(fields.completedTasksPerDay) ?
-                                                        fields.completedTasksPerDay : '0'}
-                                                    item={'COMPLETED TASKS PER DAY'}
-                                                    subItem={'(IN AVERAGE PER GOFUNDI)'}
-                                                >
-                                                    <div styleName="completed-tasks" />
-                                                </ReportRow>
-                                                <ReportRow
-                                                    styleChildren={{backgroundColor: '#fbaa1a'}}
-                                                    styleUpItem={{textOverflow: 'ellipsis', overflow: 'hidden'}}
-                                                    upItem={
-                                                        fields.activeGoFundis &&
-                                                        !isNull(fields.activeGoFundis) ?
-                                                        fields.activeGoFundis : '0'}
-                                                    item={'ACTIVE GOFUNDIS'}
-                                                    subItem={'(IN AVERAGE PER DAY)'}
-                                                >
-                                                    <div styleName="user" />
-                                                </ReportRow>
-                                            </div>
-                                        )
-                                    }),
-                                    Just: errors => (
-                                        <div>{errors}</div>
-                                    )
-                                })}
-                            </div>
-                            <div styleName="list_column" style={{marginLeft: 20}}>
-                                {props.getOverviewStats.errors.cata({
-                                    Nothing: () => props.getOverviewStats.results.cata({
-                                        Nothing: () => (
-                                            <Placeholder
-                                                style={{ width: 235, height: 190}}
-                                                busy={props.getOverviewStats.busy}
-                                                size={[ '100%', '100%' ]} />
-                                        ),
-                                        Just: fields => (
-                                            <div>
-                                                <ReportRow
-                                                    styleChildren={{backgroundColor: '#ffde00'}}
-                                                    styleUpItem={{textOverflow: 'ellipsis', overflow: 'hidden'}}
-                                                    styleReportBlock={{width: 185}}
-                                                    upItem={
-                                                        fields.averageCompletionTime &&
-                                                        !isNull(fields.averageCompletionTime) ?
-                                                        fields.averageCompletionTime : '0'}
-                                                    item={'AVERAGE TIME FOR COMPLETION'}
-                                                    subItem={'(FROM ASSIGNED TO COMPLETED)'}
-                                                >
-                                                    <div styleName="assigned-tasks" />
-                                                </ReportRow>
-                                                <ReportRow
-                                                    styleChildren={{backgroundColor: '#c21e51'}}
-                                                    styleUpItem={{textOverflow: 'ellipsis', overflow: 'hidden'}}
-                                                    styleReportBlock={{width: 185}}
-                                                    upItem={
-                                                        fields.increasePercentage &&
-                                                        !isNull(fields.increasePercentage) ?
-                                                        fields.increasePercentage : '0'}
-                                                    item={'INCREASE SINCE LAST MONTH'}
-                                                    subItem={'(TASKS COMPLETED)'}
-                                                >
-                                                    <div styleName="line-chart" />
-                                                </ReportRow>
-                                            </div>
-                                        )
-                                    }),
-                                    Just: errors => (
-                                        <div>{errors}</div>
-                                    )
-                                })}
-                            </div>
-                        </div>
+                        <OverviewBlockStats data={props.getOverviewStats}/>
                         <div style={{
                             display: 'flex',
                             flexDirection: 'row'
