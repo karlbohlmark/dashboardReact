4ee4c4c39fde60d17545e1741511283682f73aad
Evgeny Shmakov <sjs-master@yandex.ru>
2017-08-23 14:15:54 +0700

    formatDateLabel ternaryLabel fix

--- src/utils/format-date.js
+++ src/utils/format-date.js
@@ -22,6 +22,7 @@ export function formatDateTime([ date, time ]) {
 }
 
 export const getISOStringFromDate = date => new Date(date).toISOString();
+
 const getFormatString = date => date.format('YYYY-MM-DD');
 const getFormatFromDateRange = dateRange => map(item => (getFormatString(item)), dateRange);
 const containDate = curry(
@@ -37,21 +38,40 @@ const checkDates = curry(
     )
 );
 
+// export const formatDateLabel = curry(
+//     (startDate, endDate) => {
+//         if (checkDates(startDate, endDate, DATERANGE_TODAY)) {
+//             return ('Today');
+//         } else if (checkDates(startDate, endDate, DATERANGE_YESTERDAY)) {
+//             return ('Yesterday');
+//         } else if (checkDates(startDate, endDate, DATERANGE_LAST7DAYS)) {
+//             return ('Last 7 Days');
+//         } else if (checkDates(startDate, endDate, DATERANGE_LAST30DAYS)) {
+//             return ('Last 30 Days');
+//         } else if (checkDates(startDate, endDate, DATERANGE_THISMONTH)) {
+//             return ('This Month');
+//         } else if (checkDates(startDate, endDate, DATERANGE_LASTMONTH)) {
+//             return ('Last Month');
+//         }
+//         return ('Custom Range');
+//     }
+// );
+
+const ternaryLabel = curry(
+    (startDate, endDate, dateRange, fnLeft, fnRight) => (
+        !checkDates(startDate, endDate, dateRange) ?
+            fnLeft : fnRight
+    )
+);
 export const formatDateLabel = curry(
-    (startDate, endDate) => {
-        if (checkDates(startDate, endDate, DATERANGE_TODAY)) {
-            return ('Today');
-        } else if (checkDates(startDate, endDate, DATERANGE_YESTERDAY)) {
-            return ('Yesterday');
-        } else if (checkDates(startDate, endDate, DATERANGE_LAST7DAYS)) {
-            return ('Last 7 Days');
-        } else if (checkDates(startDate, endDate, DATERANGE_LAST30DAYS)) {
-            return ('Last 30 Days');
-        } else if (checkDates(startDate, endDate, DATERANGE_THISMONTH)) {
-            return ('This Month');
-        } else if (checkDates(startDate, endDate, DATERANGE_LASTMONTH)) {
-            return ('Last Month');
-        }
-        return ('Custom Range');
-    }
+    (startDate, endDate) => (
+        ternaryLabel(startDate, endDate, DATERANGE_TODAY,
+            ternaryLabel(startDate, endDate, DATERANGE_YESTERDAY,
+                ternaryLabel(startDate, endDate, DATERANGE_LAST7DAYS,
+                    ternaryLabel(startDate, endDate, DATERANGE_LAST30DAYS,
+                        ternaryLabel(startDate, endDate, DATERANGE_THISMONTH,
+                            ternaryLabel(startDate, endDate, DATERANGE_LASTMONTH, ('Custom Range'), ('Last Month')),
+                            ('This Month')), ('Last 30 Days')), ('Last 7 Days')), ('Yesterday')), ('Today'))
+    )
+
 );
